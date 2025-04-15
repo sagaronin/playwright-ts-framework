d01e132e561cb839fc6c437d7adeb377de1a5ad9
Sagar Hagwane <sagar.hagwane13@gmail.com>
2025-04-15 16:30:29 -0400

    Added new configs and comments along with support for custom report which shows execution details on github actions run job

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './src/tests',
+  //This is the default test timeout for each test
+  //If you want to set a global timeout for each test, you can do it here
+  // this can be overriden at test level, inside test spec file.
   timeout: 30000,
+  //timeout for verification steps which includes something like this expect(1).toEqual(1)
   expect: {
     timeout: 5000,
   },
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }], ['playwright-ctrf-json-reporter', {}]],
   use: {
     headless: false,
     actionTimeout: 0,
