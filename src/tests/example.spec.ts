47550fb0131b2782e22931083bf2b97981928cf6
Sagar Hagwane <sagar.hagwane13@gmail.com>
2025-04-17 17:10:38 -0400

    Added new configs in how test is structured

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,8 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../fixture/fixture';
+test.describe('Example Test Suite', () => {
 
-test('basic test', async ({ page }) => {
-    await page.goto('https://example.com');
-    const title = await page.title();
-    await page.waitForTimeout(5000); // Wait for 1 second
-    expect(title).toBe('Example Domain');
+    test.use({
+        actionTimeout:1200,//Maximum time each action such as `click()` can take
+        navigationTimeout: 1200,//Maximum time navigation command can take
+    }); 
+    // this is like before test annotation in testng
+    // this will run before each test in the test suite
+    test.beforeEach(async ({ page }) => {
+        // Navigate to the page before each test
+        await page.goto('https://example.com');
+    });
+    test('basic test', async ({ page }) => {
+        const title = await page.title();
+        await page.waitForTimeout(5000); // Wait for 1 second
+        expect(title).toBe('Example Domain');
+    });
 });
